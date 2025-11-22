44b40a69e543f7f1cb10c31f6361ed24f7eb7505
Hariton <pitsikhariton@gmail.com>
2025-11-22 11:16:34 +0400

    feat: dataset for pt

---
+++ dataset.py
@@ -0,0 +1,23 @@
+import torch
+from torch.utils.data import Dataset
+import preprocessing
+
+class ecomDataset(Dataset):
+    def __init__(self, data_table, vocab):
+        processed_labels = preprocessing.label_enc(data_table)
+        self.texts = data_table['text']
+        self.labels = processed_labels['class']
+        embeddings = preprocessing.get_embeddings(preprocessing.preprocess_text(processed_labels))
+        self.embeddings = embeddings['embeddings']
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        idx_emb = self.embeddings[idx]
+        idx_class = self.labels[idx]
+
+        emb_tensor = torch.tensor(idx_emb, dtype=torch.float32)
+        class_tensor = torch.tensor(idx_class, dtype=torch.long)
+
+        return emb_tensor, class_tensor
